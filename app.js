5f0c0d09ed93d17cb3310bbf84260588450edb25
anadtocheev <andrei_nadtocheev>
2020-12-28 20:00:27 +0000

    Add feature 1

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
 
+// Landing page
 app.get('/', (req, res) => res.send('Hello World!'))
+
+// About us page
+app.get('/about', (req, res) => res.send('Yet another about us page!'))
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
